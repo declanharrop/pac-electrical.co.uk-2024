71664aa227b2be4ebed6f499f4e558a2436b653d
Declan Harrop <dh@dhweb.dev>
2024-04-12 12:33:59 +0100

    PAC 2024

--- src/app/layout.js
+++ src/app/layout.js
@@ -97,7 +97,6 @@ export default function RootLayout({ children }) {
         <StyledComponentsRegistry>
           <Globalstyle />
           <Typography />
-          <Header />
           {children}
         </StyledComponentsRegistry>
       </body>
