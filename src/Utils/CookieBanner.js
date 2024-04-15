f17de5002314d4d9df8d24dc7bd5fe0357c70e2b
Declan Harrop <dh@dhweb.dev>
2024-04-15 13:12:53 +0100

    added stuff

--- src/Utils/CookieBanner.js
+++ src/Utils/CookieBanner.js
@@ -23,13 +23,7 @@ export default function CookieBanner() {
     setLocalStorage('cookie_consent', cookieConsent);
   }, [cookieConsent]);
   return (
-    <div
-      className={`my-10 mx-auto max-w-max md:max-w-screen-sm
-                  fixed bottom-0 left-0 right-0 
-                  flex px-3 md:px-4 py-3 justify-between items-center flex-col sm:flex-row gap-4  
-                  bg-gray-700 rounded-lg shadow z-50
-                  ${cookieConsent !== null ? 'hidden' : 'flex'}`}
-    >
+    <div className="">
       <div className="text-cente text-white-200">
         <Link href="/info/cookies">
           <p>
