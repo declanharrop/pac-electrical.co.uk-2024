266ccba3cfbff78ed46354d614b0a17dd859f27c
Declan Harrop <dh@dhweb.dev>
2024-04-15 12:58:34 +0100

    added stuff

--- src/Utils/CookieBanner.js
+++ src/Utils/CookieBanner.js
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
-import { getLocalStorage, setLocalStorage } from '@/lib/storageHelper';
+import { getLocalStorage, setLocalStorage } from '@/Lib/storageHelper';
 
 export default function CookieBanner() {
   const [cookieConsent, setCookieConsent] = useState(false);
