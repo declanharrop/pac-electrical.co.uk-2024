57f8acd219312f3201b583b5c09bbcee47f16bac
Declan Harrop <dh@dhweb.dev>
2024-04-15 10:19:05 +0100

    added stuff

--- src/app/request-callback/page.js
+++ src/app/request-callback/page.js
@@ -14,50 +14,50 @@ export default function Callback() {
           <h1>Request a callback today</h1>
         </div>
       </Hero>
-      <QuoteFormStyles>
-        <form
-          name="Request a callback"
-          method="POST"
-          data-netlify="true"
-          netlify-honeypot="bot-field"
-          // action="/information/success"
-        >
-          <p className="hidden">
-            <label>
-              Don’t fill this out if you’re human:{' '}
-              <input name="bot-field" id="bot-field" />
-            </label>
-          </p>
-          <p className="item">
-            <input
-              type="text"
-              name="name"
-              required
-              placeholder="Your Full Name"
-            />
-          </p>
-          <p className="item">
-            <input
-              type="text"
-              name="phone"
-              required
-              placeholder="Your Phone Number"
-            />
-          </p>
-          <p className="item">
-            <input
-              type="text"
-              name="email"
-              id="email"
-              required
-              placeholder="Your Email"
-            />
-          </p>
-          <p>
-            <button type="submit">Request a callback</button>
-          </p>
-        </form>
-      </QuoteFormStyles>
+      {/* <QuoteFormStyles> */}
+      <form
+        name="Request a callback"
+        method="POST"
+        data-netlify="true"
+        netlify-honeypot="bot-field"
+        // action="/information/success"
+      >
+        <p className="hidden">
+          <label>
+            Don’t fill this out if you’re human:{' '}
+            <input name="bot-field" id="bot-field" />
+          </label>
+        </p>
+        <p className="item">
+          <input
+            type="text"
+            name="name"
+            required
+            placeholder="Your Full Name"
+          />
+        </p>
+        <p className="item">
+          <input
+            type="text"
+            name="phone"
+            required
+            placeholder="Your Phone Number"
+          />
+        </p>
+        <p className="item">
+          <input
+            type="text"
+            name="email"
+            id="email"
+            required
+            placeholder="Your Email"
+          />
+        </p>
+        <p>
+          <button type="submit">Request a callback</button>
+        </p>
+      </form>
+      {/* </QuoteFormStyles> */}
     </>
   );
 }
