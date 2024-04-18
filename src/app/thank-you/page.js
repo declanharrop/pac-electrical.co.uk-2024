67cb6cd7a25edfac3eb3a7e03cc0dcd03d30a1f7
Declan Harrop <dh@dhweb.dev>
2024-04-18 19:42:50 +0100

    added whats next page

---
+++ src/app/thank-you/page.js
@@ -0,0 +1,12 @@
+import Hero from '@/Components/Hero/Hero';
+
+export default function ThankYou() {
+  return (
+    <Hero>
+      <div className="decorated-title">
+        <h1>Thank You</h1>
+        <h3>More about solar</h3>
+      </div>
+    </Hero>
+  );
+}
