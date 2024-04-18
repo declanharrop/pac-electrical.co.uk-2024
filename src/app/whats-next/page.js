872b808dae28401658c600a3ba761bccb513837e
Declan Harrop <dh@dhweb.dev>
2024-04-18 19:39:51 +0100

    added whats next page

---
+++ src/app/whats-next/page.js
@@ -0,0 +1,11 @@
+import Hero from '@/Components/Hero/Hero';
+
+export default function WhatHappensNext() {
+  return (
+    <Hero>
+      <div className="decorated-title">
+        <h1>What happens now</h1>
+      </div>
+    </Hero>
+  );
+}
