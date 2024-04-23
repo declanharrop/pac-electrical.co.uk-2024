96e3548b78b93d4bdd810979645fdab045ff3c37
Declan Harrop <dh@dhweb.dev>
2024-04-23 14:03:43 +0100

    added new stuff

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -39,179 +39,97 @@ export default function Header() {
     height: { ease: 'easeInOut', duration: 1 },
   };
 
-  if (width != null) {
-    return (
-      <HeaderStyles>
-        <div className={width < 800 ? 'mobile-social-bar' : 'social-bar'}>
-          <div className="social-links">
-            <a
-              href="https://www.facebook.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img src="/icons/facebook.svg" alt="Facebook Icon" />
-            </a>
-            <a
-              href="https://www.instagram.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img src="/icons/instagram.svg" alt="Instagram Icon" />
-            </a>
-            <a
-              href="https://www.linkedin.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img src="/icons/linkedin.svg" alt="LinkedIn Icon" />
-            </a>
-          </div>
-          <div className="contact-links">
-            <a className="call-click" href="tel:+441332552320">
-              <PhoneRoundedIcon sx={{ fontSize: '24px' }} />
-              01332 552 320
-            </a>
-            <a
-              className="email-click"
-              href="mailto:enquiries@pac-electrical.co.uk"
-            >
-              <EmailRoundedIcon sx={{ fontSize: '24px' }} />
-              enquiries@pac-electrical.co.uk
-            </a>
-          </div>
+  return (
+    <HeaderStyles>
+      <div className={width < 800 ? 'mobile-social-bar' : 'social-bar'}>
+        <div className="social-links">
+          <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
+            <img src="/icons/facebook.svg" alt="Facebook Icon" />
+          </a>
+          <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
+            <img src="/icons/instagram.svg" alt="Instagram Icon" />
+          </a>
+          <a href="https://www.linkedin.com/" target="_blank" rel="noreferrer">
+            <img src="/icons/linkedin.svg" alt="LinkedIn Icon" />
+          </a>
         </div>
-        {width < 800 ? (
-          <div className="mobile-bar">
-            <div className="logo">
-              <Link href="/">
-                <img
-                  src="/logo/pac-logo-electrical-reverse.svg"
-                  alt="PAC Electrical Logo"
-                />
-              </Link>
-            </div>
-            <MenuDrawer>
-              <div className="links">
-                <ul>
-                  <li
-                    className="services-li"
-                    onClick={() => toggleDropdown('services')}
-                  >
-                    Services
-                    {isDropdownOpen === 'services' ? (
-                      <div className="icon">
-                        <ExpandMoreRoundedIcon
-                          sx={{ fontSize: '30px', transform: 'rotate(180deg)' }}
-                        />
-                      </div>
-                    ) : (
-                      <div className="icon">
-                        <ExpandMoreRoundedIcon sx={{ fontSize: '30px' }} />
-                      </div>
-                    )}
-                  </li>
-                  <li
-                    onClick={() => {
-                      toggleDropdown('');
-                      toggleMenu();
-                    }}
-                  >
-                    <Link href="/reviews">Reviews</Link>
-                  </li>
-                  <li
-                    onClick={() => {
-                      toggleDropdown('');
-                      toggleMenu();
-                    }}
-                  >
-                    <Link href="/case-studies">Case Studies</Link>
-                  </li>
-                  <li
-                    onClick={() => {
-                      toggleDropdown('');
-                      toggleMenu();
-                    }}
-                  >
-                    <Link href="/news">News</Link>
-                  </li>
-                  <li
-                    onClick={() => {
-                      toggleDropdown('');
-                      toggleMenu();
-                    }}
-                  >
-                    <Link href="/about">About Us</Link>
-                  </li>
-                </ul>
-              </div>
-              <AnimatePresence>
-                {isDropdownOpen === 'services' && (
-                  <motion.div
-                    initial={animateFrom}
-                    animate={animateTo}
-                    exit={animateExit}
-                    transition={transition}
-                    className="mobile-dropdown"
-                  >
-                    <div className="dropdown-content">
-                      <MobileDropdownLinks />
-                    </div>
-                  </motion.div>
-                )}
-              </AnimatePresence>
-            </MenuDrawer>
+        <div className="contact-links">
+          <a className="call-click" href="tel:+441332552320">
+            <PhoneRoundedIcon sx={{ fontSize: '24px' }} />
+            01332 552 320
+          </a>
+          <a
+            className="email-click"
+            href="mailto:enquiries@pac-electrical.co.uk"
+          >
+            <EmailRoundedIcon sx={{ fontSize: '24px' }} />
+            enquiries@pac-electrical.co.uk
+          </a>
+        </div>
+      </div>
+      {width < 800 ? (
+        <div className="mobile-bar">
+          <div className="logo">
+            <Link href="/">
+              <img
+                src="/logo/pac-logo-electrical-reverse.svg"
+                alt="PAC Electrical Logo"
+              />
+            </Link>
           </div>
-        ) : (
-          <>
-            <div className="bar">
-              <div className="logo">
-                <Link href="/">
-                  <img
-                    src="/logo/pac-logo-electrical-reverse.svg"
-                    alt="PAC Electrical Logo"
-                  />
-                </Link>
-              </div>
-              <div className="links">
-                <ul>
-                  <li
-                    className="services-li header-link"
-                    // onClick={() => toggleDropdown('services')}
-                    onMouseEnter={() => hoverToggleDropdown('services')}
-                  >
-                    Services
+          <MenuDrawer>
+            <div className="links">
+              <ul>
+                <li
+                  className="services-li"
+                  onClick={() => toggleDropdown('services')}
+                >
+                  Services
+                  {isDropdownOpen === 'services' ? (
+                    <div className="icon">
+                      <ExpandMoreRoundedIcon
+                        sx={{ fontSize: '30px', transform: 'rotate(180deg)' }}
+                      />
+                    </div>
+                  ) : (
                     <div className="icon">
                       <ExpandMoreRoundedIcon sx={{ fontSize: '30px' }} />
                     </div>
-                  </li>
-                  <li
-                    onClick={() => toggleDropdown('')}
-                    className="header-link"
-                  >
-                    <Link href="/case-studies" className="header-link">
-                      Case Studies
-                    </Link>
-                  </li>
-                  <li
-                    onClick={() => toggleDropdown('')}
-                    className="header-link"
-                  >
-                    <Link href="/news">News</Link>
-                  </li>
-                  <li
-                    onClick={() => toggleDropdown('')}
-                    className="header-link"
-                  >
-                    <Link href="/about">About Us</Link>
-                  </li>
-                  <li
-                    onClick={() => toggleDropdown('')}
-                    className="header-link"
-                  >
-                    <Link href="/reviews">Reviews</Link>
-                  </li>
-                </ul>
-              </div>
+                  )}
+                </li>
+                <li
+                  onClick={() => {
+                    toggleDropdown('');
+                    toggleMenu();
+                  }}
+                >
+                  <Link href="/reviews">Reviews</Link>
+                </li>
+                <li
+                  onClick={() => {
+                    toggleDropdown('');
+                    toggleMenu();
+                  }}
+                >
+                  <Link href="/case-studies">Case Studies</Link>
+                </li>
+                <li
+                  onClick={() => {
+                    toggleDropdown('');
+                    toggleMenu();
+                  }}
+                >
+                  <Link href="/news">News</Link>
+                </li>
+                <li
+                  onClick={() => {
+                    toggleDropdown('');
+                    toggleMenu();
+                  }}
+                >
+                  <Link href="/about">About Us</Link>
+                </li>
+              </ul>
             </div>
             <AnimatePresence>
               {isDropdownOpen === 'services' && (
@@ -220,70 +138,124 @@ export default function Header() {
                   animate={animateTo}
                   exit={animateExit}
                   transition={transition}
-                  className="dropdown"
-                  onMouseLeave={() => toggleDropdown(false)}
+                  className="mobile-dropdown"
                 >
-                  <div className="dropdown-inner">
-                    <div className="dropdown-menu">
-                      <div className="logo">
-                        <Link href="/">
-                          <img
-                            src="/logo/pac-logo-electrical.svg"
-                            alt="PAC Electrical Logo"
-                          />
-                        </Link>
-                      </div>
-                      <div className="links">
-                        <ul>
-                          <li
-                            className="services-li header-link"
-                            onClick={() => toggleDropdown('services')}
-                            onMouseEnter={() => hoverToggleDropdown('services')}
-                          >
-                            Services
-                            <div className="icon">
-                              <ExpandMoreRoundedIcon
-                                sx={{ fontSize: '30px' }}
-                              />
-                            </div>
-                          </li>
-                          <li
-                            onClick={() => toggleDropdown('')}
-                            className="header-link"
-                          >
-                            <Link href="/case-studies">Case Studies</Link>
-                          </li>
-                          <li
-                            onClick={() => toggleDropdown('')}
-                            className="header-link"
-                          >
-                            <Link href="/news">News</Link>
-                          </li>
-                          <li
-                            onClick={() => toggleDropdown('')}
-                            className="header-link"
-                          >
-                            <Link href="/about">About Us</Link>
-                          </li>
-                          <li
-                            onClick={() => toggleDropdown('')}
-                            className="header-link"
-                          >
-                            <Link href="/reviews">Reviews</Link>
-                          </li>
-                        </ul>
-                      </div>
-                    </div>
-                    <div className="dropdown-content">
-                      <DropdownLinks />
-                    </div>
+                  <div className="dropdown-content">
+                    <MobileDropdownLinks />
                   </div>
                 </motion.div>
               )}
             </AnimatePresence>
-          </>
-        )}
-      </HeaderStyles>
-    );
-  }
+          </MenuDrawer>
+        </div>
+      ) : (
+        <>
+          <div className="bar">
+            <div className="logo">
+              <Link href="/">
+                <img
+                  src="/logo/pac-logo-electrical-reverse.svg"
+                  alt="PAC Electrical Logo"
+                />
+              </Link>
+            </div>
+            <div className="links">
+              <ul>
+                <li
+                  className="services-li header-link"
+                  // onClick={() => toggleDropdown('services')}
+                  onMouseEnter={() => hoverToggleDropdown('services')}
+                >
+                  Services
+                  <div className="icon">
+                    <ExpandMoreRoundedIcon sx={{ fontSize: '30px' }} />
+                  </div>
+                </li>
+                <li onClick={() => toggleDropdown('')} className="header-link">
+                  <Link href="/case-studies" className="header-link">
+                    Case Studies
+                  </Link>
+                </li>
+                <li onClick={() => toggleDropdown('')} className="header-link">
+                  <Link href="/news">News</Link>
+                </li>
+                <li onClick={() => toggleDropdown('')} className="header-link">
+                  <Link href="/about">About Us</Link>
+                </li>
+                <li onClick={() => toggleDropdown('')} className="header-link">
+                  <Link href="/reviews">Reviews</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <AnimatePresence>
+            {isDropdownOpen === 'services' && (
+              <motion.div
+                initial={animateFrom}
+                animate={animateTo}
+                exit={animateExit}
+                transition={transition}
+                className="dropdown"
+                onMouseLeave={() => toggleDropdown(false)}
+              >
+                <div className="dropdown-inner">
+                  <div className="dropdown-menu">
+                    <div className="logo">
+                      <Link href="/">
+                        <img
+                          src="/logo/pac-logo-electrical.svg"
+                          alt="PAC Electrical Logo"
+                        />
+                      </Link>
+                    </div>
+                    <div className="links">
+                      <ul>
+                        <li
+                          className="services-li header-link"
+                          onClick={() => toggleDropdown('services')}
+                          onMouseEnter={() => hoverToggleDropdown('services')}
+                        >
+                          Services
+                          <div className="icon">
+                            <ExpandMoreRoundedIcon sx={{ fontSize: '30px' }} />
+                          </div>
+                        </li>
+                        <li
+                          onClick={() => toggleDropdown('')}
+                          className="header-link"
+                        >
+                          <Link href="/case-studies">Case Studies</Link>
+                        </li>
+                        <li
+                          onClick={() => toggleDropdown('')}
+                          className="header-link"
+                        >
+                          <Link href="/news">News</Link>
+                        </li>
+                        <li
+                          onClick={() => toggleDropdown('')}
+                          className="header-link"
+                        >
+                          <Link href="/about">About Us</Link>
+                        </li>
+                        <li
+                          onClick={() => toggleDropdown('')}
+                          className="header-link"
+                        >
+                          <Link href="/reviews">Reviews</Link>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="dropdown-content">
+                    <DropdownLinks />
+                  </div>
+                </div>
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </>
+      )}
+    </HeaderStyles>
+  );
 }
