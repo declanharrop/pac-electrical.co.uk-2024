5a4da53ad662a2f413f15017e2534b18c863c81a
Declan Harrop <dh@dhweb.dev>
2024-04-23 09:25:08 +0100

    added new stuff

--- src/app/case-studies/[...slug]/page.js
+++ src/app/case-studies/[...slug]/page.js
@@ -2,6 +2,40 @@ import { getClient } from '@/Lib/client';
 import CaseStudyTemplate from '../../../Templates/CaseStudyTemplate';
 import { CASE_STUDY } from '@/Lib/queries';
 
+export async function generateMetadata({ params }) {
+  const client = getClient();
+  const pathname = params.slug[0];
+  const { data: caseStudy } = await client.query({
+    query: CASE_STUDY,
+    variables: {
+      slug: pathname,
+    },
+  });
+
+  const study = caseStudy.caseStudies[0];
+  return {
+    title: `${study.title} - Power & Control Ltd`,
+    description: study.metaDesc.substring(0, 160),
+    url: `https://pac-electrical.co.uk/case-studies/${study.slug}`,
+    type: 'article',
+    image: study.hero.url,
+    openGraph: {
+      title: `${study.title} - Power & Control Ltd`,
+      description: study.metaDesc.substring(0, 160),
+      url: `https://pac-electrical.co.uk/case-studies/${study.slug}`,
+      type: 'article',
+      article: {
+        publishedTime: study.date,
+        section: study.sector,
+      },
+      images: [
+        {
+          url: study.hero.url,
+        },
+      ],
+    },
+  };
+}
 export default async function CaseStudy({ params }) {
   const client = getClient();
   const pathname = params.slug[0];
--- src/app/news/[...slug]/page.js
+++ src/app/news/[...slug]/page.js
@@ -2,6 +2,41 @@ import { getClient } from '@/Lib/client';
 import { NEWS_STORY } from '@/Lib/queries';
 import NewsArticleTemplate from '@/Templates/NewsArticleTemplate';
 
+export async function generateMetadata({ params }) {
+  const client = getClient();
+  const pathname = params.slug[0];
+  const { data: newsArticle } = await client.query({
+    query: NEWS_STORY,
+    variables: {
+      slug: pathname,
+    },
+  });
+
+  const article = newsArticle.articles[0];
+
+  return {
+    title: `${article.title} - Power & Control Ltd`,
+    description: article,
+    url: `https://pac-electrical.co.uk/case-studies/${article.slug}`,
+    type: 'article',
+    image: article.hero.url,
+    openGraph: {
+      title: `${article.title} - Power & Control Ltd`,
+      description: article,
+      url: `https://pac-electrical.co.uk/case-studies/${article.slug}`,
+      type: 'article',
+      article: {
+        publishedTime: article.date,
+        section: article.sector,
+      },
+      images: [
+        {
+          url: article.hero.url,
+        },
+      ],
+    },
+  };
+}
 export default async function NewsArticle({ params }) {
   const client = getClient();
   const pathname = params.slug[0];
