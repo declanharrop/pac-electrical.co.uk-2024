30f58b711bec062aa31536434eda1c0ec558af31
Declan Harrop <dh@dhweb.dev>
2024-05-07 10:41:46 +0100

    added new items

---
+++ src/Lib/data/about-solar.js
@@ -0,0 +1,29 @@
+const AboutSolarData = {
+  title: 'About Solar',
+  subtitle:
+    'Solar PV Roof Systems & Solar PV Installers UK. We offer Solar PV solutions from design through to installation and maintenance. Discover our Solar PV Services!',
+  journeySections: [
+    {
+      side: 'left',
+      backgroundColor: 'var(--green)',
+      title: 'Confirming your details',
+      body: '',
+      video: {
+        title: 'Consultation Video',
+        src: 'https://www.youtube.com/embed/AVxSuKksEmU?si=CEO7dcH1vTc1p2Oj',
+      },
+    },
+    {
+      side: 'right',
+      backgroundColor: 'var(--green)',
+      title: 'Step 1 - Consultation',
+      body: 'We will visit your home for a consultation where we will assess your solar potential and discuss your energy needs and goals. This allows us to design a system perfectly tailored for you.',
+      video: {
+        title: 'Consultation Video',
+        src: 'https://www.youtube.com/embed/AVxSuKksEmU?si=CEO7dcH1vTc1p2Oj',
+      },
+    },
+  ],
+};
+
+export default AboutSolarData;
