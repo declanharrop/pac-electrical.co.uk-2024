cfb6b8756956f0c47ca8a30302a89b5fdf061326
Declan Harrop <dh@dhweb.dev>
2024-11-12 13:05:50 +0000

    gtm

---
+++ src/app/ev/page.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Link from 'next/link';
+import HeroFullHeight from '@/Components/Hero/HeroFullHeight';
+
+export default function page() {
+  return (
+    <div>
+      <HeroFullHeight
+        src="/images/2025/ev-header.webp"
+        alt="EV Charging"
+        height="100vh"
+      >
+        <h1>EV Charge Point</h1>
+        <h3 style={{ marginBottom: '30px' }}>Installation Experts</h3>
+        <Link href="/get-a-quote">
+          <button type="button" className="secondary-button">
+            Get a Quote Now
+          </button>
+        </Link>
+      </HeroFullHeight>
+    </div>
+  );
+}
